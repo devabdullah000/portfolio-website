cf76a0857ddbf0ff848a4e4b4236a75a502cf731
devabdullah000 <devabdullah000@gmail.com>
2026-09-13 23:27:58 +0500

    modifies blog page to fetch fresh data with no caching in API response.

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,7 +4,12 @@ import { personalData } from "@/utils/data/personal-data";
 import BlogCard from "../components/homepage/blog/blog-card";
 
 async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.blog_url}`)
+  const res = await fetch(
+  `https://dev.to/api/articles?username=${personalData.blog_url}`,
+  {
+    cache: 'no-store',
+  }
+)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
