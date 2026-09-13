a0533807890d34fb5fb075bd2f7f5415dba6fab5
devabdullah000 <devabdullah000@gmail.com>
2026-09-13 21:34:56 +0500

    facebook and instagram URLs updated.

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,7 @@ export const contactsData = {
     phone: '+9203231216513',
     address: 'LG-42 IT Tower Gulberg-||| Lahore',
     github: 'https://github.com/devabdullah000',
-    facebook: 'https://www.facebook.com/share/1DXEqpWyxt/',
+    facebook: 'https://www.facebook.com/share/19gBZYM6K5/',
     linkedIn: 'https://www.linkedin.com/in/iamabdullah/',
     twitter: 'https://twitter.com/iamabdullah',
     devUsername: "iamabdullah"
